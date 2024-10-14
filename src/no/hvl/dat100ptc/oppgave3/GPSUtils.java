cabb548a836b9a59eb965e27f9a0d3e940596172
580809 <brage971@gmail.com>
2024-10-14 23:13:23 +0200

    Update GPSUtils.java

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -66,27 +66,38 @@ public class GPSUtils {
 
 		double d;
 		double latitude1, longitude1, latitude2, longitude2;
+		double a, c;
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		latitude1 = toRadians(gpspoint1.getLatitude());
+		longitude1 = toRadians(gpspoint1.getLongitude());
+		latitude2 = toRadians(gpspoint2.getLatitude());
+		longitude2 = toRadians(gpspoint2.getLongitude());
+		
+		a = compute_a(latitude1, latitude2, (latitude2-latitude1), (longitude2-longitude1));
+		c = compute_c(a);
+		
+		d = R*c;
+		
+		return d;
+				
 	}
 	
 	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
-	
-		throw new UnsupportedOperationException(TODO.method());
 		
-		// TODO 
-
+		double a;
+		
+		a = (pow (sin (deltaphi/2), 2.0) + cos(phi1) * cos(phi2) * pow (sin (deltadelta/2), 2.0));
+		
+		return a;
 	}
 
 	private static double compute_c(double a) {
-
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double c;
 		
+		c = 2 * atan2 (sqrt (a), sqrt (1-a));
 		
-		// TODO 
+		return c;
 
 	}
 
@@ -96,33 +107,53 @@ public class GPSUtils {
 		int secs;
 		double speed;
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double deltaMeter = distance(gpspoint1, gpspoint2);
 		
-		// TODO
-
+		int time1 = gpspoint1.getTime();
+		int time2 = gpspoint2.getTime();
+		
+		secs = (time2 - time1);
+		
+		speed = (deltaMeter / secs);
+		
+		return speed;
+		
 	}
-
+	// "  03:02:01"
 	public static String formatTime(int secs) {
 
 		String timestr;
 		String TIMESEP = ":";
-
-		throw new UnsupportedOperationException(TODO.method());
 		
-		// TODO 
+		int timer = (secs / 3600);
+		int min = (secs % 3600)/60;
+		int sek = secs % 60;
 		
-	}
+		timestr = ("%02d%s%02d%s%02d");
+		
+		timestr = String.format(timestr, timer, TIMESEP, min, TIMESEP, sek);
+		
+		timestr = String.format("%10s", timestr);
+		
+		return timestr;
+	}	
 	
 	private static int TEXTWIDTH = 10;
 
 	public static String formatDouble(double d) {
 
-		String str;
-
+		String str = ("%10.2f");
 		
-		throw new UnsupportedOperationException(TODO.method());
+		double number = Math.round(d * 100);
+		number = number/100;
 		
-		// TODO
+		str = (number + "");
+		
+		str = String.format("%10s", str);
+		
+		return str;
+		
 		
 	}
+		
 }
